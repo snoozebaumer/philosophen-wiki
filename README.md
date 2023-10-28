994d1b1f3bc927480a880874cbe651c54e40c58c
Samuel Nussbaumer <samuel.nussbaumer@stud.hslu.ch>
2023-10-28 14:22:14 +0200

    major: - removes braces and brackets at the start to exclude them prematurely - checks for citation links ^1 so it doesn't click those - checks if link has already been visited - refactoring for clarity and performance boost - updated readme

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # philosophen-wiki
-Browser Automation. Clicks the first link on a random article on german wikipedia, as long as they aren't cursive or in braces. This goes on until timeout or until it reaches the article "Philosophie", Requires Selenium.
+
+philosophen-wiki is a browser automation project using [Selenium](https://www.selenium.dev/) automating the [Wiki Game](https://en.wikipedia.org/wiki/Wikipedia:Wiki_Game).
+
+Starting on a random article on wikipedia, it clicks on the first link, as long as they aren't cursive, in braces[^1] or lead to an external website. This goes on until a maximum amount of clicks or until it reaches the goal article, which can be defined in main.js:
+    
+```javascript 
+5: const GOAL = "Philosophie"; // The goal page
+6: const MAX_CLICKS = 200; // The maximum number of clicks before the program exits without finding the goal page
+```
+
+In order to prevent click through loops within similar topics (which the first link usually seems to lead to), the previously visited links are tracked. If the first link has already been visited, the program skips onto the next link.
+
+> :warning: Due to the simplicity of the program, it will probably not find very specific goal pages, as the first link usually leads to a more general topic.
+
+[^1]: Used to implement [Wiki Game rules](https://en.wikipedia.org/wiki/Wikipedia:Wiki_Game#The_Don'ts): Cursive links tend to be links to external sources or outside of page source. Braces usually contain dates.
+
+## How to run
+Run npm install to install selenium-webdriver:
+```bash
+npm install
+```
+Then run the program:
+```bash
+npm start
+```
--- main.js
+++ main.js
@@ -1,56 +1,127 @@
-const {Builder, By} = require('selenium-webdriver');
+const {Builder, By, until} = require('selenium-webdriver');
 const {Options} = require('selenium-webdriver/firefox');
 
+
+const GOAL = "Philosophie"; // The goal page
+const MAX_CLICKS = 200; // The maximum number of clicks before the program exits without finding the goal page
+
+const RANOOM_PAGE = "https://de.wikipedia.org/wiki/Spezial:Zuf%C3%A4llige_Seite";
+
+/**
+ * Checks if an element is clickable. A Link on wikipedia is clickable if it is visible and not underlined.
+ * @param element
+ * @returns {Promise<boolean>}
+ */
 async function isClickable(element) {
     const style = await element.getAttribute('style');
-    return style !== null && !style.includes('text-decoration: underline');
+    return style !== null && !style.includes('text-decoration: underline') && await element.isDisplayed();
 }
 
-async function doesNotContainImage(link) {
+/**
+ * Checks if an element contains an image. This is used to not be caught in an image viewer that obfuscates the article text.
+ * @param link
+ * @returns {Promise<boolean>}
+ */
+async function containsImage(link) {
     const imageTags = await link.findElements(By.tagName('img'));
-    return imageTags.length === 0;
+    return imageTags.length > 0;
 }
 
-async function isInsideParentheses(link) {
-    const parentText = await link.findElement(By.xpath('..')).getText();
-    const linkText = await link.getText();
-    console.log()
-    let regexRound = new RegExp('\\((.*' + linkText + '.*)\\)');
-    let regexSquare = new RegExp('\\[(.*' + linkText + '.*)\\]');
+/**
+ *
+ * @param link
+ * @returns {Promise<boolean>}
+ */
+async function isCitation(link) {
+    const linkHrefText = await link.getAttribute('href');
 
-    return (parentText.match(regexRound) || parentText.match(regexSquare));
+    return linkHrefText.includes('#cite_note');
 }
 
-const maxClicks = 200;
+/**
+ * Removes content within brackets or braces from the page source. Links in parentheses are explicitly excluded in the assignment.
+ * I'm guessing the reason is that they tend to be dates and locations, which aren't helpful in finding the specific goal page.
+ * Parentheses also tend to have the phonetic spelling of names in them, which would be a curve ball.
+ * May prolong the search for a different goal page.
+ * @param driver
+ * @returns {Promise<void>}
+ */
+async function removeBracesAndBrackets(driver) {
+    const pageSource = await driver.getPageSource();
+
+    /* Use regex to remove content within brackets or braces,matches most cases, there are some weird edge cases with nested braces. excludes parentheses within <a> tags as we want to click those
+    *  may need to be further optimized, as regex often misses matches in nested braces for some reason (should work in theory), But it reaches the goal page in a reasonable amount of clicks for now.
+     */
+    const cleanPageSource = pageSource.replace(/(\([^()]*\)|\[[^\[\]]*\]|\{[^{}]*\})(?![^<]*<\/a>)/g, '');
+
+    // Load the modified page source into the browser
+    await driver.executeScript('document.documentElement.innerHTML = arguments[0]', cleanPageSource);
+    await driver.sleep(1000);
+}
+
+/**
+ * Checks if the current page is the goal page.
+ * @param driver
+ * @param pageTitle
+ * @returns {Promise<*>}
+ */
+function isGoalPage(pageTitle) {
+    return pageTitle.includes(GOAL);
+}
+
+/**
+ * compares the link href to a list of visited links to check if the link has been visited before.
+ * @param linkHref
+ * @param visitedLinks
+ * @returns {*}
+ */
+function hasLinkBeenVisited(linkHref, visitedLinks) {
+    return visitedLinks.includes(linkHref);
+}
+
+function isWikipediaLink(linkHref) {
+    return linkHref.includes('wikipedia.org');
+}
+
+async function isValidLink(link, linkHrefText, visitedLinks) {
+    return await isClickable(link) && !(await containsImage(link)) && !(await isCitation(link)) && !hasLinkBeenVisited(linkHrefText, visitedLinks) && isWikipediaLink(linkHrefText);
+}
 
 (async function () {
     const options = new Options();
     const driver = await new Builder().forBrowser('firefox').setFirefoxOptions(options).build();
 
     try {
-        await driver.get('https://de.wikipedia.org/wiki/Spezial:Zuf%C3%A4llige_Seite');
-        //await driver.get('https://de.wikipedia.org/wiki/Sprachwissenschaft');
-        let clickCount = 0;
+        await driver.get(RANOOM_PAGE);
 
-        while (clickCount < maxClicks) {
-            const articleLinks = await driver.findElements(By.css('#mw-content-text a:not(#toc a,figure a, .navigation-not-searchable a, table a)')); //.navigation-not-searchable a schliesst die Weiterleitungshinweise(Artikelbeginn) aus.
+        const startPage = await driver.getTitle();
+        // are we already on the goal page? -> exit
+        if (await isGoalPage(startPage)) {
+            console.log('Already on the ' + GOAL + ' page. Exiting.');
+            return;
+        }
+
+        let visitedLinks = [];
+        let clickCount = 0;
+        while (clickCount < MAX_CLICKS) {
+            await removeBracesAndBrackets(driver);
+
+            /*
+            * .navigation-not-searchable a excludes redirection message at article start, figure a excludes text under images (placed before article text in DOM, thus excluded to emulate human behaviour),
+            * toc a excludes table of contents, table a excludes links in tables (tends to be placed before article text in DOM), i a excludes cursive links (external links, or irrelevant links)
+            * */
+            const articleLinks = await driver.findElements(By.css('#mw-content-text a:not(#toc a,figure a, .navigation-not-searchable a, table a, i a)'));
+
             let validLinks = [];
 
-
+            // only select the first 30 links to optimize performance, as there usually tends to be a valid link within the first 30
             for (let link of articleLinks.slice(0, 30)) {
-                const linkText = await link.getText();
-                console.log("System --> Link: " + linkText)
+                const linkHrefText = await link.getAttribute('href');
 
-                if (!(await isInsideParentheses(link))) {
-                    console.log("System --> textInsideParentheses --> " + linkText);
-                    if (await isClickable(link)) {
-                        console.log("System --> isClickable(link) --> " + linkText);
-                        if (await doesNotContainImage(link)) {
-                            console.log("System --> doesNotContainImage(link) --> " + linkText);
-                            validLinks.push(link);
-                            break;
-                        }
-                    }
+                if (await isValidLink(link, linkHrefText, visitedLinks)) {
+                    validLinks.push(link);
+                    visitedLinks.push(linkHrefText);
+                    break;
                 }
             }
 
@@ -61,18 +132,19 @@ const maxClicks = 200;
 
             const link = validLinks[0];
             const text = await link.getText();
-            console.log("link clicked:" + text)
+
             await link.click();
-            console.log("_______________________________________________________________________________________________________________________________________________________________________")
+            console.log("link clicked:" + text)
 
             clickCount++;
 
-            if ((await driver.getTitle()).includes('Philosophie')) {
-                console.log('Reached the "Philosophie" page. Exiting.');
+            if ((await isGoalPage(await driver.getTitle()))) {
+                console.log('Reached the ' + GOAL +  ' page in ' + clickCount + ' clicks. Exiting.');
                 break;
             }
 
-            await driver.sleep(4000); // Wait for the page to load
+            // wait for the page title to be displayed with timeout 2s, to make sure the page has loaded
+            await driver.wait(until.elementLocated(By.className('mw-page-title-main')), 2000);
         }
     } finally {
         await driver.quit();
